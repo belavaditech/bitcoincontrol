dfcb448d3f40ff1153c9d80213d2d895a9a3fa2e
Ramesh <raga2560@gmail.com>
2018-06-01 17:31:39 +0530

    prog

--- argsdoclib.js
+++ argsdoclib.js
@@ -1,4 +1,5 @@
 var options = require('options-parser');
+const fs = require('fs');
 bitcoin = require('bitcoinjs-lib');
 usage = require('./usagedoclib');
 contractinfo = require('./contract.json');
@@ -25,7 +26,7 @@ var argv = process.argv;
  var opts = {
   doccontrol:  { required: true },
   base:   { required: true , short: 'ba', default: '' },
-  contractfile:   { required: false , short: 'co', default: 'contractfile' },
+  contractfile:   { required: true , short: 'co', default: 'contractfile' },
   vendorfile:   { required: false , short: 've', default: 'vendorfile' },
   serverfile:   { required: false , short: 'sf', default: 'serverfile' },
   clientfile:   { required: false , short: 'cf', default: 'clientfile' },
@@ -33,13 +34,6 @@ var argv = process.argv;
   encryption:   { required: false , short: 'en', default: '2' },
   secret:   { required: false , short: 'se', default: '' },
   };
-
-if(argv.length < 5)
-{
-console.log("node argsdoclib.js --doccontrol --base \'{\"name\":\"bob\", \"age\":50} ' --secret \'{\"name\":false, \"age\":true} \' ");
-  options.help(opts);
-  process.exit(1);
-}
 
 function error()
 {
@@ -55,9 +49,35 @@ var result = options.parse(opts, argv, error
 );
  
 
-//console.log(result);
+console.log(result);
 var base = JSON.parse(result.opt.base) ;
 var secret = JSON.parse(result.opt.secret) ;
-console.log(base.name);
+var contractfile = result.opt.contractfile;
+if(contractfile)
+{
+var cont = require('./'+contractfile);
+console.log(JSON.stringify(cont));
+writedata(result.opt.serverfile, cont);
+}
+
+var vendorfile = result.opt.vendorfile;
+if(vendorfile)
+{
+//var cont = require('./'+vendorfile);
+//console.log(JSON.stringify(cont));
+}
+
 console.log(secret.name);
 
+function writedata(file, output)
+{
+var content = JSON.stringify(output);
+
+fs.writeFile(file+".json", content, 'utf8', function (err) {
+    if (err) {
+        return console.log(err);
+    }
+
+    console.log("The file was saved!");
+}); 
+}
